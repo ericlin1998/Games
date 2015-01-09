866448ac2a692ece2a3600316e9928d2eab9e2c5
ros_erlin <ros_erlin@WROSRM31921.seattleschools.org>
2015-01-09 08:44:07 -0800

    2

--- Games/Games/Main.java
+++ Games/Games/Main.java
@@ -1,3 +1,7 @@
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -19,7 +23,7 @@ public class Main {
 
 public class Menu extends JFrame{
 	JTextArea jtAreaOutput;
-	JButton button;
+	JButton numGame, unoGame;
 	
 	public Menu(){
 		createGui();
@@ -30,7 +34,40 @@ public class Menu extends JFrame{
 		this.setResizable(true);
 		jtAreaOutput = new JTextArea(10, 31);
 		jtAreaOutput.setEditable(false);
+		numGame = new JButton("NumberGame");
+		unoGame = new JButton("UnoGame");
+		JScrollPane scrollPane = new JScrollPane(jtAreaOutput, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		
+		GridBagLayout gridBag = new GridBagLayout();
+		Container contentPane = getContentPane();
+		contentPane.setLayout(gridBag);
+		
+		GridBagConstraints b1 = new GridBagConstraints();
+		b1.gridx = 1;
+		b1.gridy = 1;
+		contentPane.add(numGame, b1);
+		GridBagConstraints b2 = new GridBagConstraints();
+		b2.gridx = 2;
+		b2.gridy = 1;
+		contentPane.add(unoGame, b2);
+		
+		numGame.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+            	
+            }
+		});
+		unoGame.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+            	
+            }
+		});
+	}
+	
+	
+	
+	public void addText(String str){
+		jtAreaOutput.append(str);
+		jtAreaOutput.setCaretPosition(jtAreaOutput.getDocument().getLength());
 	}
 }
 
